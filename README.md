cded79c73f84a0b33f4cc3364c8b7426dc15e809
guoruibiao <marksinoberg@gmail.com>
2019-04-16 20:46:59 +0800

    初步实现一个字数统计以及选中词翻译的VSCode插件

---
+++ README.md
@@ -0,0 +1,51 @@
+# wordcount
+TODO: making a gif show.
+
+## Features
+show word number especially for Chinese, and translate selected text whether it's Chinese or english.
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Requirements
+
+If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+
+
+## Working with Markdown
+
+_**Note:**_ You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+
+### For more information
+
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,114 @@
+'use strict';
+import * as vscode from 'vscode';
+import * as WebRequest from 'web-request';
+/** reference from: 
+    wordcount: https://github.com/ycjcl868/vscode-wordcount
+    webrequest: https://www.npmjs.com/package/web-request
+**/
+
+export function activate(context : vscode.ExtensionContext) {
+    console.log('Congratulations, your extension"wordcount"is now active!');
+
+    const wordCounter = new WordCounter();
+
+    let disposable = vscode.commands.registerCommand('extension.wordCount', () => {
+        const count = wordCounter.updateWordCount();
+		if (count){ // && count >= 0) {
+            vscode.window.showInformationMessage(`字数：${count}`);
+		}
+    });
+
+    context.subscriptions.push(wordCounter);
+    context.subscriptions.push(disposable);
+}
+
+class WordCounter {
+    // VSCode 底部状态栏
+    private _statusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+    // 释放队列
+    private _disposable: vscode.Disposable;
+    // subscribe event
+    constructor() {
+        // 注册事件
+        const subscriptions: vscode.Disposable[] = [];
+        // 注册光标改变事件
+        vscode.window.onDidChangeTextEditorSelection(this.updateWordCount, this, subscriptions);
+        // 注册切换文件事件
+        vscode.window.onDidChangeActiveTextEditor(this.updateWordCount, this, subscriptions);
+        // 更新状态栏
+        this.updateWordCount();
+        // 需要释放的事件队列
+        this._disposable = vscode.Disposable.from(...subscriptions);
+    }
+    // 获取编辑器及编辑内容的上下文
+    public updateWordCount() {
+        // 获取当前编辑器对象
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            this._statusBarItem.hide();
+            return false;
+        }
+        // 当前编辑对象
+		const doc = editor.document;
+		
+		// 获取当前选中的文本
+        if(!editor){
+            return
+        }
+        let selection = editor.selection
+		let text = editor.document.getText(selection)
+		
+		// 测试显示选中的文本
+		this._translate(text);
+
+       //if (doc.languageId === 'markdown') {
+            const wordCount = this._getWordCount(doc);
+            this._statusBarItem.text = `${wordCount} Words`;
+            this._statusBarItem.show();
+            return wordCount;
+        //} else {
+        //    this._statusBarItem.hide();
+        //}
+    }
+    // 统计函数
+    public _getWordCount(doc: vscode.TextDocument): number {
+        // 当前编辑内容
+        const docContent: string = doc.getText();
+        const filterStr: string = docContent.replace(/\r\n/g, "\n");
+        // 中文字数
+        const chineseTotal: Array<string> = filterStr.match(/[\u4e00-\u9fa5]/g) || [];
+        // 匹配单字字符
+        const englishTotal: Array<string> = filterStr.match(/\b\w+\b/g) || [];
+        // 匹配数字
+        const digitalTotal: Array<string> = filterStr.match(/\b\d+\b/g) || [];
+
+		// return "字数统计：中文["+chineseTotal+"], 英文["+englishTotal+"], 数字["+digitalTotal+"], 总长度["+docContent.length+"]";
+        return (chineseTotal.length + (englishTotal.length - digitalTotal.length)) || 0;
+	}
+	
+	public _translate(keyword: string): string {
+		// 获取选中的文本
+		let wcconfig = vscode.workspace.getConfiguration("wordcount");
+		let url = wcconfig.transapi ? wcconfig.transapi : "https://fanyi.baidu.com/transapi?from=auto&to=auto&query="
+		if(keyword) {
+			url = url + encodeURI(keyword)
+			WebRequest.get(url).then(resp => {
+				let rep = JSON.parse(resp.content);
+				console.log(resp.content);
+				let transret = rep.data[0].dst;
+				this._statusBarItem.text = "[" + keyword + "]:" + transret;
+				this._statusBarItem.show();
+			});
+		}
+		return "失败了~~~~(>_<)~~~~"
+	}
+    // 当插件禁用时
+    dispose() {
+        this._statusBarItem.dispose();
+        this._disposable.dispose();
+    }
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
